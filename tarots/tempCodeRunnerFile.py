ab99629849072efa71991cfa0d58ee4dd0f82267
frarfrarf <francescatornaghi1104@gmail.com>
2023-07-07 08:50:05 +0200

    prima sistemazione prof

--- tarots/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-for button in [LETTURA_BUTTON]:
-        #         button.changeColor(MOUSE_POS)
-        #         button.update(screen)
-        
-        # for button in [SFERA_BUTTON]:
-        #         button.changeColor(MOUSE_POS)
-        #         button.update(screen)
